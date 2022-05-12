91875e42782e57cafdf1a3b14e0760218927cf31
justinrosenblum <Justin.Rosenblum@kellogg.northwestern.edu>
2022-05-12 01:54:59 +0000

    working through lab

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,21 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find_by({ "id" => params["id"] })
+    # render /views/posts/show.html.erb
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Company.new
+    @post["author"] = params["post"]["author"]
+    @post["body"] = params["post"]["body"]
+    @post["image"] = params["post"]["image"]
+    redirect_to"/posts"
+  end
+
 end
